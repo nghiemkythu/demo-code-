0013583028b652f443045a69f950e98b3352a36b
Quan Phan <hoangquanphan2906@gmail.com>
2020-06-24 14:49:34 +0700

    Add error handling

--- server.py
+++ server.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_clothes_prediction():
-    user_file = request.files.get("file")
-    prediction = predict_clothes(user_file)
-    return jsonify(prediction)
+    try:
+        user_file = request.files.get("file")
+        prediction = predict_clothes(user_file)
+        return jsonify(prediction)
+    except Exception as e:
+        return jsonify({"error": e})
 
 
 # config
